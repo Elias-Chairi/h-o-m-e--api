1abc978dfb629e112ca4a34e88c2ba5333559feb
Elias-Chairi <eliaspc@stud.ntntu.no>
2025-02-28 15:03:54 +0100

    initial java setup

---
+++ src/main/java/edu/ntnu/iir/bidata/teamHOME/Application.java
@@ -0,0 +1,15 @@
+package edu.ntnu.iir.bidata.teamHOME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main class of the application. Responsible for starting the Spring Boot application.
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
